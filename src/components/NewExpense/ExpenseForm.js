73df096192083f759f77c3977ffd4b45af6ed435
Al-E-00 <mazzellaalessio6@gmail.com>
2022-11-23 12:26:46 +0100

    Individual state slices and multiple state slices

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,8 +1,39 @@
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  //const [enteredTitle, setEnteredTitle] = useState("");
+  //const [enteredPrice, setEnteredPrice] = useState("");
+  //const [enteredDate, setEnteredData] = useState("");
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredPrice: "",
+    enteredData: "",
+  });
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+    //setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.data.value,
+    });
+  };
+
+  const amountChangeHandler = (event) => {
+    //setEnteredPrice(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredPrice: event.data.value,
+    });
+  };
+
+  const dateChangeHandler = (event) => {
+    //setEnteredData(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredData: event.data.value,
+    });
   };
 
   return (
@@ -14,11 +45,21 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="date" min="2020-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2020-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
