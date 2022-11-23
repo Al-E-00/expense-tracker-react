12d3077c6b157e6d688f44f9f197229528e2b263
Al-E-00 <mazzellaalessio6@gmail.com>
2022-11-23 12:35:14 +0100

    Correct method for handling previous state (PrevState)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -14,25 +14,46 @@ const ExpenseForm = () => {
 
   const titleChangeHandler = (event) => {
     //setEnteredTitle(event.target.value);
-    setUserInput({
+
+    //this method can sometimes lead to errors
+    /* setUserInput({
       ...userInput,
       enteredTitle: event.data.value,
+    }); */
+
+    //this is the correct method to hadle previous state
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.title };
     });
   };
 
   const amountChangeHandler = (event) => {
     //setEnteredPrice(event.target.value);
-    setUserInput({
+
+    //this method can sometimes lead to errors
+    /* setUserInput({
       ...userInput,
       enteredPrice: event.data.value,
+    }); */
+
+    //this is the correct method to hadle previous state
+    setUserInput((prevState) => {
+      return { ...prevState, enteredPrice: event.target.value };
     });
   };
 
   const dateChangeHandler = (event) => {
     //setEnteredData(event.target.value);
-    setUserInput({
+
+    //this method can sometimes lead to errors
+    /*  setUserInput({
       ...userInput,
       enteredData: event.data.value,
+    }); */
+
+    //this is the correct method to hadle previous state
+    setUserInput((prevState) => {
+      return { ...prevState, enteredData: event.target.data };
     });
   };
 
